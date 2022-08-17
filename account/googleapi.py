1cadd86bae5d35a26d7d445bea6d929dbed173ca
Kim Do Yeon <scarletkim2121@gmail.com>
2022-08-17 12:00:56 +0900

    REFACTOR: Google login api

--- account/googleapi.py
+++ account/googleapi.py
@@ -1,184 +1,56 @@
-from rest_framework.views import APIView
+from apiclient import discovery
+import httplib2
+from oauth2client import client
+from ..config.settings import SOCIAL
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-from django.shortcuts import redirect
-from django.conf import settings
-from django.contrib.auth import get_user_model
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from authenticate import jwt_login, SafeJWTAuthentication, AdministratorAuthentication
-
-
-User = get_user_model()
-
-# Mixin: 사용될 모든 api view에 필요한 접근 권한에 대한 설정을 쉽게 상속할 수 있도록 미리 만들어놓은 클래스
-
-class ApiAuthMixin:
-    authentication_classes = (SafeJWTAuthentication, )
-    permission_classes = (IsAuthenticatedOrReadOnly, )
-
-class SuperUserMixin:
-    authentication_classes = (AdministratorAuthentication, )
-    permission_classes = (IsAuthenticatedOrReadOnly, )
-
-class PublicApiMixin:
-    authentication_classes = ()
-    permission_classes = ()
+# (Receive auth_code by HTTPS POST)
+# auth_code = 
 
 
-class GoogleLoginApi(PublicApiMixin, APIView):
-    def get(self, request, *args, **kwargs):
-        app_key = settings.GOOGLE_OAUTH2_CLIENT_ID
-        scope = "https://www.googleapis.com/auth/userinfo.email " + \
-                "https://www.googleapis.com/auth/userinfo.profile"
-        
-        redirect_uri = settings.BASE_BACKEND_URL + "/account/google/login/callback"
-        google_auth_api = "https://accounts.google.com/o/oauth2/v2/auth"
-        
-        response = redirect(
-            f"{google_auth_api}?client_id={app_key}&response_type=code&redirect_uri={redirect_uri}&scope={scope}"
-        )
-        
-        return response
+# If this request does not have `X-Requested-With` header, this could be a CSRF
+if not request.headers.get('X-Requested-With'):
+    abort(403)
 
 
-class GoogleSigninCallBackApi(PublicApiMixin, APIView):
-    def get(self, request, *args, **kwargs):
-        code = request.GET.get('code')
-        google_token_api = "https://oauth2.googleapis.com/token"
-        
-        access_token = google_get_access_token(google_token_api, code)
-        user_data = google_get_user_info(access_token=access_token)
-        
-        profile_data = {
-            'username': user_data['email'],
-            'first_name': user_data.get('given_name', ''),
-            'last_name': user_data.get('family_name', ''),
-            'nickname': user_data.get('nickname', ''),
-            'name': user_data.get('name', ''),
-            'image': user_data.get('picture', None),
-            'path': "google",
-        }
-
-        
-        user, _ = social_user_get_or_create(**profile_data)
-
-        response = redirect(settings.BASE_FRONTEND_URL)
-        response = jwt_login(response=response, user=user)
-
-        return response
-
-# ***********************************************************************************
-
-# users 이용 - 사용자의 회원가입
-
-from django.db import transaction
-from django.utils import timezone
-
-from users.models import Profile
-
-
-@transaction.atomic
-def social_user_create(username, password=None, **extra_fields):
-    user = User(username=username, email=username)
-    
-    if password:
-        user.set_password(password)
-    else:
-        user.set_unusable_password()
-        
-    user.full_clean()
-    user.save()
-    
-    profile = Profile(user=user)
-    
+@api_view(['GET'])
+def google(request):
     try:
-        profile.image = extra_fields['image']
-    except:
-        pass
-    
-    if extra_fields['nickname'] == '':
-       profile.nickname = username
-    else:
-       profile.nickname = extra_fields['nickname']
-    
-    try:
-        try:
-            user.first_name = extra_fields['first_name']
-            user.last_name = extra_fields['last_name']
-        except:
-            try:
-                user.first_name = extra_fields['name']
-            except:
-                pass
-    except:
-        pass
-    
-    try:
-        path = extra_fields['path']
-        profile.signup_path = f"{path}"
-        profile.image = f"profile_image/{path}_basic.png"
-    except:
-        pass
-    
-    profile.save()
-    
-    return user
+        if(not request.GET["code"]):
+            return Response({'error': True, 'message':'code를 찾을 수 없습니다.'})
 
+        code = request.GET['code']
 
-@transaction.atomic
-def social_user_get_or_create(username, **extra_data):
-    user = User.objects.filter(email=username).first()
-    
-    if user:
-        return user, False
-    
-    return social_user_create(username=username, **extra_data), True
+        headers = {'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'}
 
- 
-# ***********************************************************************************
+        # Set path to the Web application client_secret_*.json file you downloaded
+        CLIENT_SECRET_FILE= {
+    "web":{"client_id": SOCIAL.google.client_id,
+    "project_id":SOCIAL.google.project_id,
+    "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+    "token_uri":"https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+    "javascript_origins":["http://localhost:8000","http://localhost:3000","http://copum.login.com"]
+    }
+}
 
-# google_get_access_token 함수: 구글로그인에 성공해서 받은 인증 코드(code)를 가지고 접근 토큰(access_token)을 받는 기능
-# google_get_user_info 함수: access_token을 통해서 유저 정보를 가져오는 기능
+        # Exchange auth code for access token, refresh token, and ID token
+        credentials = client.credentials_from_clientsecrets_and_code(
+    CLIENT_SECRET_FILE,
+    ['https://www.googleapis.com/auth/drive.appdata', 'profile', 'email'],
+    auth_code)
 
-import requests
-from django.core.exceptions import ValidationError
+        # Call Google API
+        http_auth = credentials.authorize(httplib2.Http())
+        drive_service = discovery.build('drive', 'v3', http=http_auth)
+        appfolder = drive_service.files().get(fileId='appfolder').execute()
 
-
-GOOGLE_ACCESS_TOKEN_OBTAIN_URL = 'https://oauth2.googleapis.com/token'
-GOOGLE_USER_INFO_URL = 'https://www.googleapis.com/oauth2/v3/userinfo'
-
-
-def google_get_access_token(google_token_api, code):
-    client_id = settings.GOOGLE_OAUTH2_CLIENT_ID
-    client_secret = settings.GOOGLE_OAUTH2_CLIENT_SECRET
-    code = code
-    grant_type = 'authorization_code'
-    redirection_uri = settings.BASE_BACKEND_URL + "/account/google/login/callback"
-    state = "random_string"
-    
-    google_token_api += \
-        f"?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type={grant_type}&redirect_uri={redirection_uri}&state={state}"
-    
-    token_response = requests.post(google_token_api)
-    
-    if not token_response.ok:
-        raise ValidationError('google_token is invalid')
-    
-    access_token = token_response.json().get('access_token')
-    
-    return access_token
-
-
-def google_get_user_info(access_token):
-    user_info_response = requests.get(
-        "https://www.googleapis.com/oauth2/v3/userinfo",
-        params={
-            'access_token': access_token
-        }
-    )
-
-    if not user_info_response.ok:
-        raise ValidationError('Failed to obtain user info from Google.')
-    
-    user_info = user_info_response.json()
-    
-    return user_info
+        # Get profile info from ID token
+        userid = credentials.id_token['sub']
+        email = credentials.id_token['email']
+        
+    except Exception as ex:
+        print("fail")
+        response = { 'error':True, 'message':ex}
+        return Response(response)
--- account/urls.py
+++ account/urls.py
@@ -1,17 +1,9 @@
 
 from django.urls import path, include
-from . import kakaoapi
-from apis import (
-    LoginApi, 
-    LogoutApi, 
-    GoogleLoginApi, 
-    GoogleSigninCallBackApi
-)
+from . import kakaoapi, googleapi
 from googleapi import *
 
 urlpatterns = [
     path('kakao/login', kakaoapi.kakao),
-    path('google/login', GoogleLoginApi.as_view(), name='google_login'),
-    path('google/callback', GoogleSigninCallBackApi.as_view(), name='google_login_callback'),
-    path('google/logout', LogoutApi.as_view(), name="logout"),
+    path('google/login', googleapi.google),
 ]
