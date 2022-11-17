44d694ee569dcb7da35ac170ea24773ea56c486c
juneunbae <dmsqo1403@naver.com>
2022-11-17 23:46:05 +0900

    ADD kakaoLogin TEST

--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import redirect
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+
 
 @api_view(['GET'])
 def getData(request):
@@ -10,3 +13,14 @@ def getData(request):
 def getData2(request):
     person = { 'name':'asdf', 'age': 21231 }
     return Response(person)
+
+
+class KakaoGetLogin(APIView) :
+    def get(self, request):
+        CLIENT_ID = 'f319f13f72f1e9a8b71e4971aa18d6aa'
+        REDIRET_URL = "http://localhost:8000/users/kakao/login/callback/"
+        url = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id={0}&redirect_uri={1}".format(
+            CLIENT_ID, REDIRET_URL)
+        res = redirect(url)
+        print(res)
+        return res
